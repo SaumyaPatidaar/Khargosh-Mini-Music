0ad0432269b5fd15397be86259c2f4151a64968d
Saumya Patidaar <gsssmaheshpurabsw@gmail.com>
2026-02-24 18:33:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = 21705136
-        self.API_HASH = "78730e89d196e160b0f1992018c6cb19"
+        self.API_ID = 34526092
+        self.API_HASH = "0de81a7870354f026d084985376becb0"
 
-        self.BOT_TOKEN = getenv("B")
-        self.MONGO_URL = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
+        self.BOT_TOKEN = getenv("8512639516:AAEARNkbtgJQsgWpPpsxC4woZSXrhi8da7E")
+        self.MONGO_URL = "mongodb+srv://khargosh:khargosh@cluster0.2fimped.mongodb.net/?appName=Cluster0"
 
         self.LOGGER_ID = -1002843633996
         self.OWNER_ID = 8319024738
@@ -18,12 +18,12 @@ class Config:
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 200000000000))
         self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 200000000000))
 
-        self.SESSION1 = getenv("S", None)
+        self.SESSION1 = getenv("BQIB5IkAv3JeyLExKg5ax49E8WhCYsm4EEvcv5zlY9_JsLq91ZOV64nxCtM94RWiK7H9IkgynPLnT9aQnU22lmlde-tyvXu4KecUWUwRTJfQmF38IYJEN6Y5atudNN-cVhcaQh-L9ADjjnxocMl6AF0q7Stm-wG5vFF1JGRjlgDSOvM1l_4A-LwFiy9lrkULMW2AyoQ6nEwAWx2Z4QzogkcciiAAEP4pkBGMZaEoiz45U2TxfIRauj9bvUMGdtMAU35OlzAwnkkSnaiovS7U6IexzjK9iqTbXIx1ijmXqyZOlKy1NIHXMxHU3S9Ik0BdkQTfurcWc6fZ4nSpDEdb8oefxqpVygAAAAHuQUkQAA", None)
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = "https://t.me/Toxic_bots"
-        self.SUPPORT_CHAT = "https://t.me/toxiic_chats"
+        self.SUPPORT_CHANNEL = "https://t.me/khargosh_updates"
+        self.SUPPORT_CHAT = "https://t.me/khargosh_support"
 
         self.AUTO_END: bool = getenv("AUTO_END", True)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
